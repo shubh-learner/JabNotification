2dd75e3c2daf2c0ea8e13e9b8e1c0de92ddf1b4f
Shubh Arya <56305668+shubh-learner@users.noreply.github.com>
2021-05-07 00:57:36 +0530

    first commit

---
+++ JabNotification.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 17:05:07 2021
+
+@author: Shubh Arya
+"""
+
+import requests
+import time
+import datetime
+
+
+while True:
+    date = datetime.datetime.now()
+    dateToday = date.strftime("%d-%m-%Y")
+    url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=363&date={}'.format(dateToday)
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.51"})
+    Dict = response.json() 
+    msg = ""
+
+    for x in range(len(Dict['centers'])):
+        if (Dict['centers'][x]['sessions'][0]['min_age_limit'] == 18 and Dict['centers'][x]['sessions'][0]['available_capacity'] >= 1 ):
+            locmsg = ( "\nPIN Code:       " +  str(Dict['centers'][x]['pincode']) + "\n" +
+                "Address:        " + Dict['centers'][x]['address'] + "\n" +
+                "Vaccine Name:   " + Dict['centers'][x]['sessions'][0]['vaccine'] + "\n" +
+                "Age Limit:      " + str(Dict['centers'][x]['sessions'][0]['min_age_limit']) + "\n" +
+                "Number of Slots:" + str(Dict['centers'][x]['sessions'][0]['available_capacity']) + "\n" +
+                "DATE available: " + str(Dict['centers'][x]['sessions'][0]['date'])+ "\n" +
+                
+                "*****************************************" )
+            msg+= locmsg
+
+    baseURL = 'https://api.telegram.org/bot1859416700:AAFmOu1hoHLqcHmCenLMzlKUfLU6GBQkCjo/sendMessage?chat_id=-577156311&text={}'.format(msg)
+    requests.get(baseURL)
+    time.sleep(20)
+
+
+        
+        
+
+
---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python JabNotification.py
+
