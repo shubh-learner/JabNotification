7eb23ba62dac743fde3cb5f376c6ec8a1f8642ed
Shubh Arya <56305668+shubh-learner@users.noreply.github.com>
2021-05-07 12:16:24 +0530

    update

--- JabNotification.py
+++ JabNotification.py
@@ -15,7 +15,11 @@ while True:
     dateToday = date.strftime("%d-%m-%Y")
     url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=363&date={}'.format(dateToday)
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.51"})
-    Dict = response.json() 
+    try:
+        Dict = response.json()
+    except ValueError:
+        print("Response content is not valid JSON")
+     
     msg = ""
 
     for x in range(len(Dict['centers'])):
