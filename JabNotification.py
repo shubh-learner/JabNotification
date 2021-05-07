8828cdbec4ba1d9913a65f7c06410dcbd8357173
Shubh Arya <56305668+shubh-learner@users.noreply.github.com>
2021-05-07 12:22:21 +0530

    update

--- JabNotification.py
+++ JabNotification.py
@@ -17,26 +17,28 @@ while True:
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.51"})
     try:
         Dict = response.json()
+        msg = ""
+
+        for x in range(len(Dict['centers'])):
+            if (Dict['centers'][x]['sessions'][0]['min_age_limit'] == 45 and Dict['centers'][x]['sessions'][0]['available_capacity'] >= 40 ):
+                locmsg = ( "\nPIN Code:       " +  str(Dict['centers'][x]['pincode']) + "\n" +
+                          "Address:        " + Dict['centers'][x]['address'] + "\n" +
+                          "Vaccine Name:   " + Dict['centers'][x]['sessions'][0]['vaccine'] + "\n" +
+                          "Age Limit:      " + str(Dict['centers'][x]['sessions'][0]['min_age_limit']) + "\n" +
+                          "Number of Slots:" + str(Dict['centers'][x]['sessions'][0]['available_capacity']) + "\n" +
+                          "DATE available: " + str(Dict['centers'][x]['sessions'][0]['date'])+ "\n" +
+                          
+                          "*****************************************" )
+                msg+= locmsg
+
+        baseURL = 'https://api.telegram.org/bot1859416700:AAFmOu1hoHLqcHmCenLMzlKUfLU6GBQkCjo/sendMessage?chat_id=-577156311&text={}'.format(msg)
+        requests.get(baseURL)
+        time.sleep(20)
+    
     except ValueError:
         print("Response content is not valid JSON")
      
-    msg = ""
-
-    for x in range(len(Dict['centers'])):
-        if (Dict['centers'][x]['sessions'][0]['min_age_limit'] == 45 and Dict['centers'][x]['sessions'][0]['available_capacity'] >= 40 ):
-            locmsg = ( "\nPIN Code:       " +  str(Dict['centers'][x]['pincode']) + "\n" +
-                "Address:        " + Dict['centers'][x]['address'] + "\n" +
-                "Vaccine Name:   " + Dict['centers'][x]['sessions'][0]['vaccine'] + "\n" +
-                "Age Limit:      " + str(Dict['centers'][x]['sessions'][0]['min_age_limit']) + "\n" +
-                "Number of Slots:" + str(Dict['centers'][x]['sessions'][0]['available_capacity']) + "\n" +
-                "DATE available: " + str(Dict['centers'][x]['sessions'][0]['date'])+ "\n" +
-                
-                "*****************************************" )
-            msg+= locmsg
-
-    baseURL = 'https://api.telegram.org/bot1859416700:AAFmOu1hoHLqcHmCenLMzlKUfLU6GBQkCjo/sendMessage?chat_id=-577156311&text={}'.format(msg)
-    requests.get(baseURL)
-    time.sleep(20)
+ 
 
 
         
